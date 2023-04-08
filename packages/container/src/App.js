c731f592d88335d34c77a63675a7ad33cdc225ca
Priyanka Singh <nidhibsp.singh@gmail.com>
2023-04-08 17:10:26 +0530

    Added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hi there weclome new !!!!</h1>
-      <hr />
-      <MarketingApp />;
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />;
+      </div>
+    </BrowserRouter>
   );
 }
